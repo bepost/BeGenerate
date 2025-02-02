934485af0ba1d7a3c93d35ad80321a3475eefe2f
Joepb <git@bepost.nl>
2025-02-02 13:17:19 +0400

    Add unit tests

---
+++ BeGenerate.Tests/AutoInterfaceGeneratorTests.cs
@@ -0,0 +1,55 @@
+using System.Linq;
+using System.Threading.Tasks;
+using BeGenerate.Generators.AutoInterface;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using VerifyXunit;
+using Xunit;
+
+namespace BeGenerate.Tests;
+
+public sealed class AutoInterfaceGeneratorTests
+{
+    private static GeneratorDriver BuildDriver(params string[] sources)
+    {
+        var compilation = CSharpCompilation.Create(
+            "compilation",
+            sources.Select(s => CSharpSyntaxTree.ParseText(s)),
+            [
+                MetadataReference.CreateFromFile(typeof(object).Assembly.Location),
+                MetadataReference.CreateFromFile(typeof(AutoInterfaceAttribute).Assembly.Location)
+            ],
+            new CSharpCompilationOptions(OutputKind.DynamicallyLinkedLibrary));
+
+        var generator = new AutoInterfaceGenerator();
+        var driver = CSharpGeneratorDriver.Create(generator);
+        return driver.RunGenerators(compilation);
+    }
+
+    [Fact]
+    public Task Driver()
+    {
+        var driver = BuildDriver();
+        return Verifier.Verify(driver);
+    }
+
+    [Fact]
+    public Task RunResult()
+    {
+        var driver = BuildDriver(
+            """
+            using BeGenerate;
+
+            namespace TestNamespace;
+
+            [AutoInterface]
+            public class MyClass
+            {
+                public string GetMessage() => "Hello";
+                public int Add(int a, int b) => a + b;
+            }
+            """);
+        var results = driver.GetRunResult();
+        return Verifier.Verify(results.Results);
+    }
+}
--- BeGenerate.Tests/UnitTest1.cs
+++
@@ -1,9 +0,0 @@
-namespace BeGenerate.Tests;
-
-public class UnitTest1
-{
-    [Fact]
-    public void Test1()
-    {
-    }
-}
