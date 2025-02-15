6d276a70b9b25886788ffd37631fa216b94d33c3
Joep Beusenberg <joep@bepost.nl>
2025-02-15 21:18:53 +0400

    Support explicit default values (#7)

--- BeGenerate/Generators/AutoInterface/ParameterData.cs
+++ BeGenerate/Generators/AutoInterface/ParameterData.cs
@@ -13,18 +13,52 @@ internal sealed record ParameterData
     {
         Type = symbol.Type.ToDisplayString();
         Name = symbol.Name.EscapeKeyword();
+        DefaultValue = symbol.HasExplicitDefaultValue
+            ? FormatDefaultValue(symbol.ExplicitDefaultValue, symbol.Type)
+            : null;
     }
 
+    public string? DefaultValue { get; }
     private string Name { get; }
     private string Type { get; }
 
     public string Emit()
     {
-        return $"{Type} {Name}";
+        var cb = new CodeBuilder();
+        cb.Append($"{Type} {Name}");
+        cb.AppendIf(DefaultValue != null, $" = {DefaultValue}");
+        return cb.ToString();
     }
 
     public static IEnumerable<ParameterData> From(IMethodSymbol symbol)
     {
         return symbol.Parameters.Select(p => new ParameterData(p));
+    }
+
+    private static string FormatDefaultValue(object? value, ITypeSymbol type)
+    {
+        if (value == null)
+            return "default";
+
+        return type.SpecialType switch
+        {
+            SpecialType.System_String => $"\"{value}\"",
+            SpecialType.System_Char => $"'{value}'",
+            SpecialType.System_Boolean => value.ToString()
+                                              ?.ToLowerInvariant() ??
+                                          "false",
+            SpecialType.System_Single => $"{value}f",
+            SpecialType.System_Double => $"{value}d",
+            SpecialType.System_Decimal => $"{value}m",
+            SpecialType.System_Int64 => $"{value}L",
+            SpecialType.System_UInt64 => $"{value}UL",
+            SpecialType.System_Int32 => $"{value}",
+            SpecialType.System_UInt32 => $"{value}U",
+            SpecialType.System_Int16 => $"{value}",
+            SpecialType.System_UInt16 => $"{value}",
+            SpecialType.System_Byte => $"{value}",
+            SpecialType.System_SByte => $"{value}",
+            _ => value.ToString() ?? "default"
+        };
     }
 }
