561894ecc4e1d1783991d9f7b97cd6ec347e7ecc
Joepb <git@bepost.nl>
2025-03-01 17:52:21 +0400

    Fix arrow getters

--- BeGenerate/Helpers/IEnumerableExtensions.cs
+++ BeGenerate/Helpers/IEnumerableExtensions.cs
@@ -5,8 +5,11 @@ namespace BeGenerate.Helpers;
 
 internal static class IEnumerableExtensions
 {
-    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T> action)
+    public static void ForEach<T>(this IEnumerable<T>? enumerable, Action<T> action)
     {
+        if (enumerable is null)
+            return;
+
         foreach (var item in enumerable)
             action(item);
     }
