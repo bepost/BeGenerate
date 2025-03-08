1b32c20fb2d0fa39810a96e27bcf2ba5029a0926
Joepb <git@bepost.nl>
2025-03-08 10:39:46 +0400

    Fix sample build warnings

--- BeGenerateSample/SampleService.cs
+++ BeGenerateSample/SampleService.cs
@@ -9,22 +9,6 @@ namespace BeGenerateSample;
 [Implements<IEnumerable>]
 internal class SampleService : Test
 {
-    [Obsolete]
-    public string Message { [Obsolete("get")] get; [Obsolete("get")] set; }
-
-    [ExcludeFromInterface]
-    public int Test { get; set; }
-
-    public int Add(int a, int b)
-    {
-        return a + b;
-    }
-
-    public string GetMessage()
-    {
-        return "Hello from SampleService!";
-    }
-
     [DoesNotReturn]
     IEnumerator IEnumerable.GetEnumerator()
     {
